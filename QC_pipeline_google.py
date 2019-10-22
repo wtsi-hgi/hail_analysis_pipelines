2ac6d34a228e207f69362634dc0503ead20f0f7a
Pavlos Antoniou <pa10@sanger.ac.uk>
2019-10-22 17:08:41 +0100

    no gz for interval file

--- QC_pipeline_google.py
+++ QC_pipeline_google.py
@@ -117,13 +117,13 @@ if __name__ == "__main__":
     mt1 = mt1.annotate_cols(sample_QC_nonHail=sample_QC_nonHail.key_by("ID")[mt1.s])
     print("Filtering on sample qc")
     mt_sqc1_filtered = mt1.filter_cols(
-        (mt1.sample_QC_nonHail.PASS_Depth == 1) &
-        (mt1.sample_QC_nonHail.PASS_ID == 1) &
-        (mt1.sample_QC_nonHail.PASS_Median_FreeMix == 1) &
-        (mt1.sample_QC_nonHail.PASS_NRD == 1) &
-        (mt1.sample_QC_nonHail.PASS_SampleSwap == 1) &
-        (mt1.sample_QC_nonHail.PASS_Sex == 1) &
-        (mt1.sample_QC_nonHail.PASS_DUP == 1)
+        (mt1.sample_QC_nonHail["PASS.Depth"] == 1) &
+        (mt1.sample_QC_nonHail["PASS.ID"] == 1) &
+        (mt1.sample_QC_nonHail["PASS.Median.FreeMix"] == 1) &
+        (mt1.sample_QC_nonHail["PASS.NRD"] == 1) &
+        (mt1.sample_QC_nonHail["PASS.SampleSwap"] == 1) &
+        (mt1.sample_QC_nonHail["PASS.Sex"] == 1) &
+        (mt1.sample_QC_nonHail["PASS.DUP"] == 1)
     )
     print("Writing out filtered sample qc checkpoint")
     mt_sqc2 = hl.sample_qc(mt_sqc1_filtered, name='sample_QC_Hail')
