599ea0361e5dece2acfc98735f727bd122889765
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-01-20 13:45:00 +0000

    updated export vCF

--- WGS_QC/export_VCF_WGS_rsID_hwe.py
+++ WGS_QC/export_VCF_WGS_rsID_hwe.py
@@ -34,13 +34,31 @@ with open(f"{thresholds}", 'r') as f:
     thresholds = json.load(f)
 
 CHROMOSOMES= [
-               "chr18",
-               "chr19",
-               "chr20",
-               "chr21",
-               "chr22",
-               "chrX",
-               "chrY"
+               "chr1",
+               "chr2",
+               "chr3",
+               "chr4",
+               "chr5",
+               "chr6",
+               "chr7",
+               "chr8",
+               "chr9",
+               "chr10",
+               "chr11",
+               "chr12",
+               "chr13",
+               "chr14",
+               "chr15",
+               "chr16",
+               "chr17"
+
+               #"chr18", #done
+               #"chr19",
+               #"chr20",
+               #"chr21",
+               #"chr22",
+               #"chrX",
+               #"chrY"
 ]
 
 if __name__ == "__main__":
