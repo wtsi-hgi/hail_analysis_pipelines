4687ecf13d63211ece5f2bc43824407d8fcd7ab2
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-01-28 16:03:19 +0000

    correct AC AN frequency for sex chromosomes

--- WGS_QC/correct_frequency_sex_chrs.py
+++ WGS_QC/correct_frequency_sex_chrs.py
@@ -33,6 +33,46 @@ with open(f"{thresholds}", 'r') as f:
     thresholds = json.load(f)
 
 
+def annotate_allele_counts_chrX(mt):
+    #annotate samples with gender information from sampleQC table, change "ID" and "SEX"column names appropriately:
+    #mt=matrixtable.annotate_cols(sex=sample_info_table.key_by("ID")[matrixtable.s].SEX)
+    #calculate call stats(AC,AN, AF filtered by sex and if it's in PAR region)
+    #mt=mt.annotate_rows(call_stats_male= hl.agg.filter(mt.sex == 1, hl.agg.call_stats(mt.GT, mt.alleles)))
+    mt=mt.annotate_rows(call_stats_male_NONPAR= hl.agg.filter(((mt.sex == 1) & (mt.locus.in_x_nonpar())), hl.agg.call_stats(mt.GT, mt.alleles)))
+    mt=mt.annotate_rows(call_stats_male_PAR= hl.agg.filter(((mt.sex == 1) & (mt.locus.in_x_par())), hl.agg.call_stats(mt.GT, mt.alleles)))
+    mt=mt.annotate_rows(call_stats_female= hl.agg.filter(mt.sex == 2, hl.agg.call_stats(mt.GT, mt.alleles)))
+    #Halve AC for males and halve AN for males and create 2 new fields maleAC, maleAN
+    mt=mt.annotate_rows(maleAC_NONPAR = mt.call_stats_male_NONPAR.AC.map(lambda x:hl.int32(x/2)))
+    mt=mt.annotate_rows(maleAN_NONPAR = hl.int32(mt.call_stats_male_NONPAR.AN / 2))
+    #Add the new AC and AN for males with the AC,AN for females together to get the final count per variant:
+    #final_AC and final_AN have the final values
+    mt=mt.annotate_rows(final_AC= mt.maleAC_NONPAR + mt.call_stats_male_PAR.AC + mt.call_stats_female.AC)
+    mt=mt.annotate_rows(final_AN= mt.maleAN_NONPAR + mt.call_stats_male_PAR.AN + mt.call_stats_female.AN) 
+    mt=mt.annotate_rows(frequency=hl.float64(mt.final_AC[1]/mt.final_AN))
+    fields_to_drop=['call_stats_male_NONPAR','call_stats_male_PAR','call_stats_female','maleAC_NONPAR','maleAN_NONPAR']
+    mt=mt.drop(*fields_to_drop)
+    return mt;
+
+
+def annotate_allele_counts_chrY(mt):
+    #annotate samples with gender information from sampleQC table, change "ID" and "SEX"column names appropriately:
+    #mt=matrixtable.annotate_cols(sex=sample_info_table.key_by("ID")[matrixtable.s].SEX)
+    #calculate call stats(AC,AN, AF filtered by sex and if it's in PAR region)
+    #mt=mt.annotate_rows(call_stats_male= hl.agg.filter(mt.sex == 1, hl.agg.call_stats(mt.GT, mt.alleles)))
+    mt=mt.annotate_rows(call_stats_male= hl.agg.filter(mt.sex == 1 , hl.agg.call_stats(mt.GT, mt.alleles)))
+    mt=mt.annotate_rows(call_stats_female= hl.agg.filter(mt.sex == 2, hl.agg.call_stats(mt.GT, mt.alleles)))
+    #Halve AC for males and halve AN for males and create 2 new fields maleAC, maleAN
+    mt=mt.annotate_rows(maleAC = mt.call_stats_male.AC.map(lambda x:hl.int32(x/2)))
+    mt=mt.annotate_rows(maleAN = hl.int32(mt.call_stats_male.AN / 2))
+    #Add the new AC and AN for males with the AC,AN for females together to get the final count per variant:
+    #final_AC and final_AN have the final values
+    mt=mt.annotate_rows(final_AC= mt.maleAC + mt.call_stats_female.AC)
+    mt=mt.annotate_rows(final_AN= mt.maleAN + mt.call_stats_female.AN) 
+    mt=mt.annotate_rows(frequency=hl.float64(mt.final_AC[1]/mt.final_AN))
+    fields_to_drop=['call_stats_male','call_stats_female','maleAC','maleAN']
+    mt=mt.drop(*fields_to_drop)
+    return mt;
+
 if __name__ == "__main__":
     #need to create spark cluster first before intiialising hail
     sc = pyspark.SparkContext()
