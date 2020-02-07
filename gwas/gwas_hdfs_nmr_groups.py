91751b8120e3aef173abcb889d19055f3d4ec9ae
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-02-07 17:51:57 +0000

    added covariates

--- gwas/gwas_hdfs_nmr_groups.py
+++ gwas/gwas_hdfs_nmr_groups.py
@@ -159,7 +159,7 @@ if __name__ == "__main__":
         print(nmr2_new)
         i=i+1
         gwas = hl.linear_regression_rows(
-            y=[nmr_new],
+            y=nmr_new,
             x=mt.GT.n_alt_alleles(), covariates=[1.0]+covariates_array, pass_through=[mt.rsid])
         fields_to_drop = ['sum_x', 'y_transpose_x','t_stat' ]
         gwas_table=gwas.drop(*fields_to_drop)
@@ -168,19 +168,19 @@ if __name__ == "__main__":
         gwas_table=gwas_table.annotate(ALT=gwas_table.alleles[1])
         gwas_table=gwas_table.annotate(AF=mt.rows()[gwas_table.locus, gwas_table.alleles].variant_QC_Hail.AF[1])
         print(" Writing gwas table checkpoint")
-        if pheno != 'nmr_acace':
-            gwas = gwas_table.checkpoint(f"{tmp_dir}/gwas/{project}-{dataset}-gwas-nmr-{i}.table", overwrite=True)
-        else:
-            gwas=hl.read_table("{tmp_dir}/gwas/{project}-{dataset}-gwas-nmr-{i}.table")
+        
+        gwas = gwas_table.checkpoint(f"{tmp_dir}/gwas/{project}-{dataset}-gwas-nmr-{i}.table", overwrite=True)
+        
+        gwas=hl.read_table("{tmp_dir}/gwas/{project}-{dataset}-gwas-nmr-{i}.table")
         print("Exporting tsv table")
         gwas.export(f"{tmp_dir}/gwas/{project}-{dataset}-gwas-nmr-{i}.tsv.bgz", header=True)
         for j in range(len(nmr_new)):
             print(f"Plotting manhattan {j}:{nmr2_new[j]}")
-            p = hl.plot.manhattan(gwas_table.p_value[j][0], title=f"{nmr2_new[j]} GWAS")
+            p = hl.plot.manhattan(gwas_table.p_value[j], title=f"{nmr2_new[j]} GWAS")
             output_file(f"{temp_dir}/gwas/WGS-manhattan-{nmr2_new[j]}.html", mode='inline')
             save(p)
             print(f"Plotting QQ plot for {j} - {nmr2_new[j]}")    
-            q = hl.plot.qq(gwas_table.p_value[j][0], collect_all=False, n_divisions=100, title=f"{nmr2_new[j]} QQ plot")
+            q = hl.plot.qq(gwas_table.p_value[j], collect_all=False, n_divisions=100, title=f"{nmr2_new[j]} QQ plot")
             output_file(f"{temp_dir}/gwas/{project}-{dataset}-{nmr2_new[j]}-QQplot.html", mode='inline')
             save(q)
     nmr_new=[]
