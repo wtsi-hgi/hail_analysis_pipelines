d1743d979df5c8b0f7e6690dcf96b128efe89e9a
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-02-07 14:40:05 +0000

    added covariates

--- gwas/gwas_hdfs_nmr_groups.py
+++ gwas/gwas_hdfs_nmr_groups.py
@@ -168,9 +168,9 @@ if __name__ == "__main__":
         gwas_table=gwas_table.annotate(ALT=gwas_table.alleles[1])
         gwas_table=gwas_table.annotate(AF=mt.rows()[gwas_table.locus, gwas_table.alleles].variant_QC_Hail.AF[1])
         print(" Writing gwas table checkpoint")
-        gwas = gwas_table.checkpoint(f"{tmp_dir}/gwas/{project}-{dataset}-gwas-nmr-{i}.table")
+        gwas = gwas_table.checkpoint(f"{tmp_dir}/gwas/{project}-{dataset}-gwas-nmr-{i}.table", overwrite=True)
         print("Exporting tsv table")
-        gwas.export(f"{tmp_dir}/gwas/{project}-{dataset}-gwas-nmr-{i}.tsv.bgz", header=True)
+        gwas.export(f"{tmp_dir}/gwas/{project}-{dataset}-gwas-nmr-{i}.tsv.bgz", header=True, overwrite=True)
         for j in range(len(nmr_new)):
             print(f"Plotting manhattan {j}:{nmr2_new[j]}")
             p = hl.plot.manhattan(gwas_table.p_value[j][0], title=f"{nmr2_new[j]} GWAS")
