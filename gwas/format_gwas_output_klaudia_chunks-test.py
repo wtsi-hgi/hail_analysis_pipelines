e504e144f414497c7b9837dedf27ec63eb2f43d6
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-02-11 10:24:42 +0000

    gwas hdfs nmr groups part 2

--- gwas/format_gwas_output_klaudia_chunks-test.py
+++ gwas/format_gwas_output_klaudia_chunks-test.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
         
         for i, x in chunk.groupby('nmr_phenotypes'):
             p = os.path.join(outpath, "INT-WGS-gwas-nmr-{}_test.csv.gz".format(i.lower()))
-            x.to_csv(p, sep="\t", header=(not os.path.exists(tsvout)),compression='gzip',index=False, mode='a')
+            x.to_csv(p, sep="\t", header=(not os.path.exists(tsvout)),compression='gzip',index=True, mode='a')
 
         #chunk.to_csv(tsvout, sep="\t", header=(not os.path.exists(tsvout)),compression='gzip', index=True ,mode='a' )
 
