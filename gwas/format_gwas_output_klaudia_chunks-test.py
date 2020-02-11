b96573a970cfb21c4a9da77cdadd7f520dbf7598
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-02-11 10:12:05 +0000

    gwas hdfs nmr groups part 2

--- gwas/format_gwas_output_klaudia_chunks-test.py
+++ gwas/format_gwas_output_klaudia_chunks-test.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
         #chunk_list.append(chunk)
         #chunk.columns=['locus',	'alleles',	'rsid',	'n', 'beta','standard_error','p_value',	'nmr_phenotypes','REF',	'ALT','AF']
         
-        chunk.to_csv(tsvout, sep="\t", header=(not os.path.exists(tsvout)),compression='gzip', index=False ,mode='a' )
+        chunk.to_csv(tsvout, sep="\t", header=(not os.path.exists(tsvout)),compression='gzip', index=True ,mode='a' )
 
     #df_concat=pd.concat(chunk_list)
     #df.to_csv(tsvout, sep="\t",compression='gzip', header=True, index=False )
