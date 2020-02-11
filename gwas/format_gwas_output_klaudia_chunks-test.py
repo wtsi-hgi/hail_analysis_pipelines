081baff847541a4b6c2d1dfd3c2107e1309f7009
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-02-11 09:32:58 +0000

    gwas hdfs nmr groups part 2

--- gwas/format_gwas_output_klaudia_chunks-test.py
+++ gwas/format_gwas_output_klaudia_chunks-test.py
@@ -17,8 +17,8 @@ import gzip
 
 #'["blbal"]'.strip("[]")
 
-tsv1="/lustre/scratch119/realdata/mdt2/projects/interval_wgs/analysis/hail_analysis/gwas/nmr_results/tables/10lines.txt.gz"
-tsvout="/lustre/scratch119/realdata/mdt2/projects/interval_wgs/analysis/hail_analysis/gwas/nmr_results/tables/test.bgz"
+tsv1="/lustre/scratch119/realdata/mdt2/projects/interval_wgs/analysis/hail_analysis/gwas/nmr_results/tables/10lines_long.txt.gz"
+tsvout="/lustre/scratch119/realdata/mdt2/projects/interval_wgs/analysis/hail_analysis/gwas/nmr_results/tables/test.txt.bgz"
 
 if __name__ == "__main__":
 
@@ -28,9 +28,10 @@ if __name__ == "__main__":
     for chunk in df_chunk:
     #df= pd.read_csv(tsv1, delimiter="\t", compression='gzip')
         chunk=chunk.drop(columns=['alleles'])
-        chunk[['beta','standard_error','p_value','nmr_phenotypes']]=chunk[['beta','standard_error','p_value','nmr_phenotypes']].apply(lambda x: x.str.strip("[]"), axis=1)
-        
-        chunk[['nmr_phenotypes']]=chunk[['nmr_phenotypes']].apply(lambda x: x.str.strip("\"\""), axis=1)
+       # chunk[['beta','standard_error','p_value','nmr_phenotypes']]=chunk[['beta','standard_error','p_value','nmr_phenotypes']].apply(lambda x: x.str.strip("[]"), axis=1)
+
+        chunk=chunk.explode(['beta','standard_error','p_value','nmr_phenotypes'])
+       # chunk[['nmr_phenotypes']]=chunk[['nmr_phenotypes']].apply(lambda x: x.str.strip("\"\""), axis=1)
         #chunk_list.append(chunk)
         #chunk.columns=['locus',	'alleles',	'rsid',	'n', 'beta','standard_error','p_value',	'nmr_phenotypes','REF',	'ALT','AF']
         
