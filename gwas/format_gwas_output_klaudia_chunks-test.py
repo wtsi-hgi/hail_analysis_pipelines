a67a0d0853e75a6039c5ffc0f916f51f738e170d
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-02-11 10:23:19 +0000

    gwas hdfs nmr groups part 2

--- gwas/format_gwas_output_klaudia_chunks-test.py
+++ gwas/format_gwas_output_klaudia_chunks-test.py
@@ -24,31 +24,7 @@ from ast import literal_eval
 
 tsv1="/lustre/scratch119/realdata/mdt2/projects/interval_wgs/analysis/hail_analysis/gwas/nmr_results/tables/10lines_long.txt.gz"
 tsvout="/lustre/scratch119/realdata/mdt2/projects/interval_wgs/analysis/hail_analysis/gwas/nmr_results/tables/test.txt.bgz"
-def explode(df, lst_cols, fill_value=''):
-    # make sure `lst_cols` is a list
-    if lst_cols and not isinstance(lst_cols, list):
-        lst_cols = [lst_cols]
-    # all columns except `lst_cols`
-    idx_cols = df.columns.difference(lst_cols)
-
-    # calculate lengths of lists
-    lens = df[lst_cols[0]].str.len()
-
-    if (lens > 0).all():
-        # ALL lists in cells aren't empty
-        return pd.DataFrame({
-            col:np.repeat(df[col].values, df[lst_cols[0]].str.len())
-            for col in idx_cols
-        }).assign(**{col:np.concatenate(df[col].values) for col in lst_cols}) \
-          .loc[:, df.columns]
-    else:
-        # at least one list in cells is empty
-        return pd.DataFrame({
-            col:np.repeat(df[col].values, df[lst_cols[0]].str.len())
-            for col in idx_cols
-        }).assign(**{col:np.concatenate(df[col].values) for col in lst_cols}) \
-          .append(df.loc[lens==0, idx_cols]).fillna(fill_value) \
-          .loc[:, df.columns]
+outpath="/lustre/scratch119/realdata/mdt2/projects/interval_wgs/analysis/hail_analysis/gwas/nmr_results/tables"
 
 if __name__ == "__main__":
 
@@ -70,7 +46,11 @@ if __name__ == "__main__":
         #chunk_list.append(chunk)
         #chunk.columns=['locus',	'alleles',	'rsid',	'n', 'beta','standard_error','p_value',	'nmr_phenotypes','REF',	'ALT','AF']
         
-        chunk.to_csv(tsvout, sep="\t", header=(not os.path.exists(tsvout)),compression='gzip', index=True ,mode='a' )
+        for i, x in chunk.groupby('nmr_phenotypes'):
+            p = os.path.join(outpath, "INT-WGS-gwas-nmr-{}_test.csv.gz".format(i.lower()))
+            x.to_csv(p, sep="\t", header=(not os.path.exists(tsvout)),compression='gzip',index=False, mode='a')
+
+        #chunk.to_csv(tsvout, sep="\t", header=(not os.path.exists(tsvout)),compression='gzip', index=True ,mode='a' )
 
     #df_concat=pd.concat(chunk_list)
     #df.to_csv(tsvout, sep="\t",compression='gzip', header=True, index=False )
