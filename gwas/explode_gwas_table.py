e0563779932d6d2265b375a950194dd4747e6520
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-02-17 23:55:54 +0000

    udpate output file in explode gwas table

--- gwas/explode_gwas_table.py
+++ gwas/explode_gwas_table.py
@@ -54,7 +54,7 @@ if __name__ == "__main__":
         chunk=chunk.set_index(['locus', 'rsid' , 'REF','ALT','n','AF']).apply(lambda x: x.apply(pd.Series).stack()).reset_index(level=1, drop=True)
         #locus, rsid, REF, ALT, n, AF, beta, standard_error, p_value, nmr_phenotypes
         #chunk=chunk.drop(columns=['level_5'])
-        chunk=chunk.reset_index()
+        #chunk=chunk.reset_index()
         print(chunk)
         #chunk=chunk.explode(['beta','standard_error','p_value','nmr_phenotypes'])
        # chunk[['nmr_phenotypes']]=chunk[['nmr_phenotypes']].apply(lambda x: x.str.strip("\"\""), axis=1)
