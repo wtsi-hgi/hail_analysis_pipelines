97e65786b546d09f81b93480a83d8060150a6e6e
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-03-16 12:49:54 +0000

    gwas/farm_scripts/check_completed_nmrs.py

--- gwas/farm_scripts/check_completed_nmrs.py
+++ gwas/farm_scripts/check_completed_nmrs.py
@@ -8,7 +8,7 @@ import re
 plots_path="/lustre/scratch119/humgen/projects/interval_wgs/analysis/hail_analysis/gwas/nmr_results/gwas_tables"
 allpaths=[p for p in pathlib.Path(plots_path).iterdir() if p.is_file()]
 
-#print(allpaths)
+print(allpaths)
 
 data=[]
 nmr_phenotypes='/lustre/scratch119/realdata/mdt2/projects/interval_wgs/analysis/hail_analysis/gwas/nmr_results/gwas_tables/nmr_pheno_names.txt'
