7f4aaf777439666f2f12233a2dd06486a2bd4606
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-03-16 12:43:32 +0000

    gwas/farm_scripts/check_completed_nmrs.py

---
+++ gwas/farm_scripts/check_completed_nmrs.py
@@ -0,0 +1,34 @@
+import pandas as pd 
+import os
+import json
+import pathlib
+from pathlib import Path
+import re
+#open path 
+plots_path="/lustre/scratch119/humgen/projects/interval_wgs/analysis/hail_analysis/gwas/nmr_results/tables/final_tables"
+allpaths=[p for p in pathlib.Path(plots_path).iterdir() if p.is_file()]
+
+#print(allpaths)
+
+data=[]
+nmr_phenotypes='/lustre/scratch119/realdata/mdt2/projects/interval_wgs/analysis/hail_analysis/gwas/nmr_results/gwas_tables/nmr_pheno_names.txt'
+with open(nmr_phenotypes) as f:
+    lines = [line.rstrip() for line in f]
+
+for path in allpaths:
+    #print(path)
+    info={}
+
+    filename=path.stem
+    #print(path.stem)
+    m=re.search(r'INT-WGS-gwas-nmr-(.*)\.(\w+).tsv', filename)
+    if m:
+        name=m.group(1)
+        #print(name)
+        if name not in lines:
+            print (name)
+
+
+
+    #print(filename)
+
