574667ad39b95f6814fc31fcf4a7b82794b13939
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-03-23 14:50:57 +0000

    remove dups

--- gwas/remove_dups_awk.sh
+++ gwas/remove_dups_awk.sh
@@ -4,5 +4,6 @@ conda init bash
 conda activate pa10
 
 
-
-zcat /lustre/scratch119/humgen/projects/interval_wgs/analysis/hail_analysis/gwas/nmr_results/gwas_tables/INT-WGS-gwas-nmr-nmr_lldlpl_.tsv.bgz | awk '!seen[$0]++' | gzip > /lustre/scratch119/humgen/projects/interval_wgs/analysis/hail_analysis/gwas/nmr_results/gwas_tables/remove_duplicates/INT-WGS-gwas-nmr-nmr_lldlpl_.tsv.bgz 
+file=$1
+name=$2
+zcat ${file} | awk '!seen[$0]++' | gzip > /lustre/scratch119/humgen/projects/interval_wgs/analysis/hail_analysis/gwas/nmr_results/gwas_tables/remove_duplicates/${name}
