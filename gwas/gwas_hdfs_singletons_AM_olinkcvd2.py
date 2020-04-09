4ffe40d3a77b44aaf7589ac363bc1641521650b4
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-04-09 09:44:02 +0100

    olinkinf final phenotypes

--- gwas/gwas_hdfs_singletons_AM_olinkcvd2.py
+++ gwas/gwas_hdfs_singletons_AM_olinkcvd2.py
@@ -133,8 +133,8 @@ if __name__ == "__main__":
     #dbsnp_rows = dbsnp.rows()
 
     CHROMOSOME="WGS"
-    #mt = hl.read_matrix_table(f"{temp_dir}/intervalwgs/WGS_final_march_2020_dbsnp_v53.mt")
-    mt = hl.read_matrix_table("s3a://intervalwgs/wgs-matrixtable/WGS_final_march_2020_dbsnp_v53.mt")
+    mt = hl.read_matrix_table(f"{temp_dir}/intervalwgs/WGS_final_march_2020_dbsnp_v53.mt")
+    #mt = hl.read_matrix_table("s3a://intervalwgs/wgs-matrixtable/WGS_final_march_2020_dbsnp_v53.mt")
     print("Number of initial variants:")
     print(mt.count())
     
