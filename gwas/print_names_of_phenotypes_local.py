7ffdb2236ad0d497f77511304aa81f0e8548846f
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-05-04 15:05:44 +0100

    print fbc sysmex phenotype names

--- gwas/print_names_of_phenotypes_local.py
+++ gwas/print_names_of_phenotypes_local.py
@@ -13,13 +13,13 @@ from pathlib import Path
 from bokeh.plotting import output_file, save, show
 from datetime import datetime
 
-project_root=Path(__file__).parent.parent.parent
+project_root = Path(__file__).parent.parent.parent
 print(project_root)
 
 s3credentials = os.path.join(project_root, "config_files/s3_credentials.json")
 print(s3credentials)
 
-storage = os.path.join(project_root , "config_files/storage.json")
+storage = os.path.join(project_root, "config_files/storage.json")
 
 thresholds = os.path.join(project_root, "config_files/thresholds.json")
 
@@ -33,77 +33,78 @@ with open(f"{thresholds}", 'r') as f:
     thresholds = json.load(f)
 
 
+project = "INT"
+dataset = "WGS"
 
-project="INT"
-dataset="WGS"
-
-nmr=[]
-sysmex=[]
-metabolon_metabolomics=[]
-olink_inf=[]
-olink_cvd2=[]
-olink_cvd3=[]
-olink_neu=[]
-somalogic_proteomics=[]
-fbc=[]
-pcas=[]
-nmr2=[]
-sysmex2=[]
-olink2_inf=[]
-olink2_cvd2=[]
-olink2_cvd3=[]
-olink2_neu=[]
-somalogic2=[]
-fbc2=[]
-pcas_names=[]
-covariates_array=[]
-covariates_names=[]
-covariates_olinkf=[]
-covariates_olinkf_names=[]
-covariates_olinkcvd2=[]
-covariates_olinkcvd2_names=[]
-covariates_olinkcvd3=[]
-covariates_olinkcvd3_names=[]
-covariates_olinkneu=[]
-covariates_olinkneu_names=[]
+nmr = []
+sysmex = []
+metabolon_metabolomics = []
+olink_inf = []
+olink_cvd2 = []
+olink_cvd3 = []
+olink_neu = []
+somalogic_proteomics = []
+fbc = []
+pcas = []
+nmr2 = []
+sysmex2 = []
+olink2_inf = []
+olink2_cvd2 = []
+olink2_cvd3 = []
+olink2_neu = []
+somalogic2 = []
+fbc2 = []
+pcas_names = []
+covariates_array = []
+covariates_names = []
+covariates_olinkf = []
+covariates_olinkf_names = []
+covariates_olinkcvd2 = []
+covariates_olinkcvd2_names = []
+covariates_olinkcvd3 = []
+covariates_olinkcvd3_names = []
+covariates_olinkneu = []
+covariates_olinkneu_names = []
 
 if __name__ == "__main__":
-    #need to create spark cluster first before intiialising hail
+    # need to create spark cluster first before intiialising hail
     sc = pyspark.SparkContext()
-    #Define the hail persistent storage directory
+    # Define the hail persistent storage directory
     tmp_dir = "hdfs://spark-master:9820/"
     temp_dir = os.path.join(os.environ["HAIL_HOME"], "tmp")
-    now= datetime.now()
+    now = datetime.now()
 
-    hl.init(sc=sc, tmp_dir=tmp_dir, default_reference="GRCh38", log=temp_dir +f'/logfile-{now}.log')
-    #s3 credentials required for user to access the datasets in farm flexible compute s3 environment
+    hl.init(sc=sc, tmp_dir=tmp_dir, default_reference="GRCh38",
+            log=temp_dir + f'/logfile-{now}.log')
+    # s3 credentials required for user to access the datasets in farm flexible compute s3 environment
     # you may use your own here from your .s3fg file in your home directory
     hadoop_config = sc._jsc.hadoopConfiguration()
 
     hadoop_config.set("fs.s3a.access.key", credentials["mer"]["access_key"])
     hadoop_config.set("fs.s3a.secret.key", credentials["mer"]["secret_key"])
-     
-    phenotypes = hl.import_table("s3a://intervalwgs-qc/phenotypes_wgs_wes_fbc_sysmex_nmr_olink_somalogic_50-wes-removed_05-03-2020.csv", impute=True, delimiter=',')
+
+    phenotypes = hl.import_table(
+        "s3a://intervalwgs-qc/phenotypes_wgs_wes_fbc_sysmex_nmr_olink_somalogic_50-wes-removed_05-03-2020.csv", impute=True, delimiter=',')
     #dbsnp = hl.import_vcf("s3a://intervalwgs-qc/GCF_000001405.38.fixed.vcf.gz", force_bgz=True, reference_genome='GRCh38',skip_invalid_loci=True)
     #dbsnp_rows = dbsnp.rows()
 
-    CHROMOSOME="WGS"
+    CHROMOSOME = "WGS"
 
-    f = open(f"{temp_dir}/scripts/hail-pipelines-internal/hail_analysis_pipelines/gwas/phenotype_lists/olink_phenotype_names.txt", "w")
-    mt = hl.read_matrix_table(f"{temp_dir}/intervalwgs/WGS_final_march_2020_dbsnp_v53.mt")
-    
+    f = open(f"{temp_dir}/scripts/hail-pipelines-internal/hail_analysis_pipelines/gwas/phenotype_lists/fbc_sysmex_phenotype_names.txt", "w")
+    mt = hl.read_matrix_table(
+        f"{temp_dir}/intervalwgs/WGS_final_march_2020_dbsnp_v53.mt")
+
     print("Number of initial variants:")
     print(mt.count())
-    
+
     print("Annotating matrixtable with phenotypes:")
-    phenotypes=phenotypes.key_by('ID')
+    phenotypes = phenotypes.key_by('ID')
     mt = mt.annotate_cols(phenotype=phenotypes[mt.s])
-    
 
     print("Grouping the phenotypes into lists:")
-    ph1=list(mt.phenotype)
+    ph1 = list(mt.phenotype)
     for pheno in ph1:
-        if pheno =='BWA' or pheno =='Study':
+        if pheno == 'BWA' or pheno == 'Study':
             covariates_array.append(hl.float64(mt.phenotype[pheno]))
             covariates_names.append(pheno)
         if pheno.startswith('somalogic'):
@@ -150,14 +151,16 @@ if __name__ == "__main__":
             pcas.append(mt.phenotype[pheno])
             pcas_names.append(pheno)
 
-    covariates_array=pcas+covariates_array
-    covariates_names=pcas_names+covariates_names
+    covariates_array = pcas+covariates_array
+    covariates_names = pcas_names+covariates_names
 
     #all_groups=[nmr,somalogic_proteomics,sysmex,olink_inf, olink_cvd2,olink_cvd3,olink_neu, fbc]
     #all_names=[nmr2,somalogic2,somalogic2, sysmex2, olink2_inf, olink2_cvd2,olink2_cvd3,olink2_neu, fbc2]
 
-    all_groups=[olink_inf, olink_cvd2,olink_cvd3,olink_neu]
-    all_names=[olink2_inf, olink2_cvd2,olink2_cvd3,olink2_neu]
+    #all_groups=[olink_inf, olink_cvd2,olink_cvd3,olink_neu]
+    #all_names=[olink2_inf, olink2_cvd2,olink2_cvd3,olink2_neu]
+    all_groups = [fbc, sysmex]
+    all_names = [fbc2, sysmex2]
     for list_name in all_names:
         for phenotype in list_name:
             print(phenotype)
