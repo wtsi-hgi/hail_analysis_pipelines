232c0dfed1757f27ccbaa594b539e2e3645adb0e
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-08-19 12:33:19 +0100

    added gwas to bed file metabolon scripts

---
+++ gwas/farm_scripts/gwas_to_bed_metabolon_xaa.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+# bsub -q long -G hgi -R'select[mem>10000] rusage[mem=10000]' -M10000  -o gwas_bed_addpheno.o -e gwas_bed_addpheno.e  ./gwas_to_bed_addpheno.sh 
+
+CHUNK=/lustre/scratch119/realdata/mdt2/projects/interval_wgs/analysis/hail_analysis/gwas/final_results/gwas_tables/metabolon/xaa
+
+folder=/lustre/scratch119/realdata/mdt2/projects/interval_wgs/analysis/hail_analysis/gwas/final_results/gwas_tables/metabolon
+outdir=${folder}/bed_files
+mkdir -p $outdir
+while read -r FILE; do
+	COUNT=$(( $COUNT + 1 ))
+	fbname=$(basename "${FILE}" .tsv.bgz)
+    zless "${FILE}"  |tr ':' '\t' > "${outdir}"/interimxaa.bed
+	#s3cmd put "${FILE}" "${BUCKET}/${CHROMOSOME}-vcf/${gs_name}"
+    awk 'BEGIN{FS=OFS="\t"} {$2 = $2 OFS $2} 1' ${outdir}/interimxaa.bed| tail -n+2| gzip -c > ${outdir}/${fbname}.bed.gz
+    rm ${outdir}/interimxaa.bed
+	echo ${COUNT}
+done < "${CHUNK}"
+
