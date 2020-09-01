91534664a99b68ab8857b43faaeca2c664ae8c4f
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-09-01 13:41:14 +0100

    tabix addpheno

---
+++ gwas/farm_scripts/tabix_addpheno.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+# bsub -q long -G hgi -R'select[mem>10000] rusage[mem=10000]' -M10000  -o gwas_bed_addpheno.o -e gwas_bed_addpheno.e  ./gwas_to_bed_addpheno.sh 
+
+folder=/lustre/scratch119/realdata/mdt2/projects/interval_wgs/analysis/hail_analysis/gwas/final_results/gwas_tables/addpheno/bed_files
+outdir=${folder}/bed_files
+mkdir -p $outdir
+
+for f in ${folder}/*.bed.gz
+do 
+tabix -p $f 
+done
