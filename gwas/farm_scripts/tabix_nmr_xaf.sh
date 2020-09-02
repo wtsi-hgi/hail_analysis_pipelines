0fe61615832ec33408f268fd9116737957866b16
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-09-02 10:41:03 +0100

    tabix nmr

--- gwas/farm_scripts/tabix_nmr_xaf.sh
+++ gwas/farm_scripts/tabix_nmr_xaf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 # bsub -q long -G hgi -R'select[mem>10000] rusage[mem=10000]' -M10000  -o gwas_bed_addpheno.o -e gwas_bed_addpheno.e  ./gwas_to_bed_addpheno.sh 
-
+# 
 CHUNK=/lustre/scratch119/realdata/mdt2/projects/interval_wgs/analysis/hail_analysis/gwas/final_results/gwas_tables/nmr/bed_files/xaf
 
 folder=/lustre/scratch119/realdata/mdt2/projects/interval_wgs/analysis/hail_analysis/gwas/final_results/gwas_tables/nmr/bed_files
