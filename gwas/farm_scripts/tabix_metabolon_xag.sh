621db82212ebef2021f9d34ae6522700b61d6b36
Pavlos Antoniou <pa10@sanger.ac.uk>
2020-09-02 10:51:58 +0100

    tabix metabolon

--- gwas/farm_scripts/tabix_metabolon_xag.sh
+++ gwas/farm_scripts/tabix_metabolon_xag.sh
@@ -2,7 +2,7 @@
 
 # bsub -q long -G hgi -R'select[mem>10000] rusage[mem=10000]' -M10000  -o gwas_bed_addpheno.o -e gwas_bed_addpheno.e  ./gwas_to_bed_addpheno.sh 
 # 
-CHUNK=/lustre/scratch119/realdata/mdt2/projects/interval_wgs/analysis/hail_analysis/gwas/final_results/gwas_tables/metabolon/bed_files/xaa
+CHUNK=/lustre/scratch119/realdata/mdt2/projects/interval_wgs/analysis/hail_analysis/gwas/final_results/gwas_tables/metabolon/bed_files/xag
 
 folder=/lustre/scratch119/realdata/mdt2/projects/interval_wgs/analysis/hail_analysis/gwas/final_results/gwas_tables/metabolon/bed_files
 outdir=${folder}
